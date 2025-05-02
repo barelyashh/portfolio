6cc36be2678229260edde29f14b36f7cd4121a6f
Yash Soni <yash.soni@cadt-solutions.com>
2025-05-02 16:38:51 +0530

    WIP

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', 
   plugins: [react()],
     build: {
         chunkSizeWarningLimit: 100000000
     },
-    base: "/3d-react-portfolio",
 })
